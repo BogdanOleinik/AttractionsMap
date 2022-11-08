5a45a484750e0b6cab158f1e24de259e2d7d121e
Bogdan Oleinik <bogdan.oleynik@joy-dev.com>
2022-11-08 18:34:15 +0300

    Initial Commit

---
+++ AttractionsMap/AttractionsMapApp.swift
@@ -0,0 +1,17 @@
+//
+//  AttractionsMapApp.swift
+//  AttractionsMap
+//
+//  Created by Богдан Олейник on 08.11.22.
+//
+
+import SwiftUI
+
+@main
+struct AttractionsMapApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

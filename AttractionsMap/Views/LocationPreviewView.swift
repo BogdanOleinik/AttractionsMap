5d8c96d05f8e70c58ff71a3965584b4b76ed44dc
Bogdan Oleinik <bogdan.oleynik@joy-dev.com>
2022-11-15 13:00:20 +0800

    final version

--- AttractionsMap/Views/LocationPreviewView.swift
+++ AttractionsMap/Views/LocationPreviewView.swift
@@ -77,7 +77,7 @@ extension LocationPreviewView {
     
     private var learnMore: some View {
         Button {
-            
+            viewModel.sheetLocation = location
         } label: {
             Text("Learn more")
                 .font(.headline)
